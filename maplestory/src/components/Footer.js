b8e329f1f3467bd5a993e167a30320616bfcaffd
frostyral <csjjgarcia@gmail.com>
2022-09-05 14:55:58 +0800

    [Navbar Mobile Responsive]

--- maplestory/src/components/Footer.js
+++ maplestory/src/components/Footer.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import '../styles/Footer.sass';
+import logo from "../media/nexon_logo.png"
 
 function Footer() {
     //const classes = styles()
     return (
         <footer class="">
-                <div class="container-fluid">
+                <div>
                  <ul>
+                 
                     <li><a href="">Your Account</a></li>
                     <li><a href="">Get Support</a></li>
                     <li><a href="">About Nexon</a></li>
                     <li><a href="">Privacy Policy</a></li>
                 </ul> 
                 
-  </div>
+                <ul>
+                <img href="" class="logo" src={logo} alt="Logo"/>
+                <a>© 2022 Talandral Inc. All Rights Reserved</a>
+                </ul> 
+                    
+                
+                
+                
+                
+            
+                </div>
+                 
 </footer>
     )
 }
--- maplestory/src/components/Navbar.js
+++ maplestory/src/components/Navbar.js
@@ -5,11 +5,11 @@ import logo from "../media/logo.png"
 function NavBar() {
    
     return (
-        <nav class="navbar sticky-top bg-dark">
-            
+        <nav class="navbar">
+                
                  <ul>
-                    <img href="" class="logo" src={logo} alt="Logo"/>
                     
+                 <img href="" class="logo" src={logo} alt="Logo"/>
                     <li><a href="">News</a></li>
                     <li><a href="">The Game</a></li>
                     <li><a href="">Community</a></li>
@@ -17,13 +17,15 @@ function NavBar() {
                     <li><a href="">Buy Merch</a></li>
 
                     <social>
-                    <a href="https://www.facebook.com/maplestory"><i class="fab fa-facebook-f"></i></a>
-                    <a href="https://twitter.com/maplestory"><i class="fab fa-twitter"></i></a>
-                    <a href="https://www.youtube.com/user/MapleStory"><i class="fab fa-youtube"></i></a>
-                    <a href="https://www.instagram.com/maplestory/"><i class="fab fa-instagram"></i></a>
-                    <a href="https://discord.gg/maplestory"><i class="fab fa-discord"></i></a>
-                    <a href="https://www.twitch.tv/maplestory"><i class="fab fa-twitch"></i></a>
-                    <a href="https://www.tiktok.com/@maplestory"><i class="fab fa-tiktok"></i></a>
+                        <socialbuttons>
+                        <a href="https://www.facebook.com/maplestory"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://twitter.com/maplestory"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.youtube.com/user/MapleStory"><i class="fab fa-youtube"></i></a>
+                        <a href="https://www.instagram.com/maplestory/"><i class="fab fa-instagram"></i></a>
+                        <a href="https://discord.gg/maplestory"><i class="fab fa-discord"></i></a>
+                        <a href="https://www.twitch.tv/maplestory"><i class="fab fa-twitch"></i></a>
+                        <a href="https://www.tiktok.com/@maplestory"><i class="fab fa-tiktok"></i></a>
+                        </socialbuttons>
                     </social>
 
                     <button href="">PLAY FREE NOW</button> 
