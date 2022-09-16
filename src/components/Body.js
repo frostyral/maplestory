048ed2605b204439e253ee43627c62c4f008e2be
frostyral <csjjgarcia@gmail.com>
2022-09-16 15:17:48 +0800

    Added Third News Box and Polished Code

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,18 +3,21 @@ import '../styles/Home.sass';
 import header_image from '../media/header-image-two.jpg';
 import news_default from '../media/news_default_two.png';
 import news_community from '../media/news_community.png';
+import news_maintenance from '../media/news_maintenance.png';
 
 function Body() {
   return (
     <body>
       <div class="header-home">
         <img src={header_image} class="header-image" />
-        <button
-          href="https://maplestory.nexon.net/"
-          class="slideshow-button"
-        >
-          PLAY MAPLESTORY NOW
-        </button>
+        <a>
+          <button
+            href="https://maplestory.nexon.net/"
+            class="slideshow-button"
+          >
+            PLAY MAPLESTORY NOW
+          </button>
+        </a>
       </div>
       <div class="section-news">
         <div class="box-update-news">
@@ -46,7 +49,7 @@ function Body() {
             <div class="card-textbox">
               <a
                 class="card-title"
-                href="https://maplestory.nexon.net/news/76652/content-closure-notice"
+                href="https://maplestory.nexon.net/news/76653/livestream-potion-mixing-contest-results-w-cm-ezrabell"
               >
                 Livestream - Potion Mixing Contest Results w/ CM
                 Ezrabell!
@@ -54,10 +57,32 @@ function Body() {
               <p>
                 Find out the winners of the Potion Mixing Contest and
                 see CM Ezrabell recreate the winning recipies!
-                <p class="timestamp">1 HOUR AGO</p>
+                <p class="timestamp">9 HOURS AGO</p>
                 <a
                   class="card-button"
-                  href="https://maplestory.nexon.net/news/76652/content-closure-notice"
+                  href="https://maplestory.nexon.net/news/76653/livestream-potion-mixing-contest-results-w-cm-ezrabell"
+                >
+                  READ MORE
+                </a>
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img class="card-image" src={news_maintenance} />
+            <div class="card-textbox">
+              <a
+                class="card-title"
+                href="https://maplestory.nexon.net/news/76635/completed-unscheduled-channel-maintenance-september-13-2022"
+              >
+                Unsheduled Channel Maintenance - September 13, 2022
+              </a>
+              <p>
+                The maintenance has been completed and all channels
+                are now available, Thank you for your patience.
+                <p class="timestamp">SEPTEMBER 13, 2022</p>
+                <a
+                  class="card-button"
+                  href="https://maplestory.nexon.net/news/76635/completed-unscheduled-channel-maintenance-september-13-2022"
                 >
                   READ MORE
                 </a>
