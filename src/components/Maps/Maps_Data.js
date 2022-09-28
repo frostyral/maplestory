f2ff96bad5fe75f0f749174d9786a8b8f902605a
frostyral <csjjgarcia@gmail.com>
2022-09-28 10:36:01 +0800

    Sorted Maps Data

--- src/components/Maps/Maps_Data.js
+++ src/components/Maps/Maps_Data.js
@@ -7,15 +7,6 @@ export default [
       'El Nath is a snowy town that can be accessed via the first floor of Orbis Tower. Past El Nath is the volcano-like Dungeon area, which can only be accessed if one is level 50 or above.',
     imageUrl: './media/elnath.png',
   },
-
-  {
-    title: 'New Leaf City',
-    continent: 'MASTERIA',
-    level: 'Level 100 - 110',
-    description:
-      'New Leaf City is a town founded on the ruins of an ancient city, called Old Sappe Village, that sunk into the ocean over 1000 years ago. It housed a technologically advanced civilization that used powerful techniques to perform good acts.',
-    imageUrl: './media/newleaf.jpg',
-  },
   {
     title: 'Aquarium',
     continent: 'AQUA ROAD',
@@ -23,5 +14,13 @@ export default [
     description:
       'Aqua Road is an underwater region, accessible via an entrance at the base of Orbis Tower. Aquarium, known for its popular zoo, is Aqua Roads main city and lies at the bottom of the seabed. Many sea creatures and monsters can be found here, including ruthless Sharks.',
     imageUrl: './media/aquarium.png',
+  },
+  {
+    title: 'New Leaf City',
+    continent: 'MASTERIA',
+    level: 'Level 100 - 110',
+    description:
+      'New Leaf City is a town founded on the ruins of an ancient city, called Old Sappe Village, that sunk into the ocean over 1000 years ago. It housed a technologically advanced civilization that used powerful techniques to perform good acts.',
+    imageUrl: './media/newleaf.jpg',
   },
 ];
